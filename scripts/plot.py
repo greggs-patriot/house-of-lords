2718945abc6a16fcd2bc5f5702d83dba19abe9a9
greggs-patriot <rests_fours_0r@icloud.com>
2026-02-20 00:28:34 +0000

    refactor

--- scripts/plot.py
+++ scripts/plot.py
@@ -2,14 +2,13 @@ import os
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
 import matplotlib.dates as mdates
-from matplotlib import font_manager
 
 IN_PATH = os.path.join('interim','lords_count.csv')
 OUT_PATH = os.path.join('out','lords_membership.png')
 
-def default_settings(fig : plt.Figure, ax : plt.Axes):
+def default_settings():
+    fig, ax = plt.subplots(figsize=(10, 6))
     bg_colour = '#fff9f5'
     
     fig.patch.set_facecolor(bg_colour)
@@ -20,23 +19,20 @@ def default_settings(fig : plt.Figure, ax : plt.Axes):
     ax.margins(x=0.025)
 
     ax_right = ax.twinx()
-    ax_right.set_ylim(ax.get_ylim())
+    
 
     ax_right.spines['top'].set_visible(False)
 
- 
-    #for spine in ax.spines.values():
-     #   spine.set_linewidth(1.5)
 
     ax.spines['left'].set_linewidth(1.5)
     ax.spines['bottom'].set_linewidth(1.5)
-
     ax_right.spines['right'].set_linewidth(1.5)
-    #ax_right.spines['bottom'].set_linewidth(1.5)
+    
+    ax.tick_params(axis='y', labelsize=20)
+    ax_right.tick_params(axis='y', labelsize=20)
+    ax.tick_params(axis='x', labelsize=20)
 
-    #for spine in ax_right.spines.values():
-    #    spine.set_linewidth(1.5)
-
+    return fig,ax,ax_right
 
 
 df = pd.read_csv(IN_PATH)
@@ -46,16 +42,14 @@ df['date'] = pd.to_datetime(df['date'])
 df = df.sort_values('date')
 df = df[(df['date'] >= '2000-01-01') & (df['date'] <= '2025-12-31')]
 
-rcParams['font.size'] = 20
-fig, ax = plt.subplots(figsize=(10, 6))
+
+fig, ax, ax_right = default_settings()
 
 ax.plot(df['date'], df['total'], label='Total', color='#c00000', linewidth=1.5)
-default_settings(fig,ax)
-#ax.plot(df['date'], df['total_male'], label='Male')
-#ax.plot(df['date'], df['total_female'], label='Female')
+ax_right.set_ylim(ax.get_ylim())
 
-# Labels
-ax.set_title('House of Lords Membership numbers', x=0.03, loc='left')
+
+ax.set_title('House of Lords Membership numbers', x=0.03, loc='left',fontsize=24)
 ax.xaxis.set_major_locator(mdates.YearLocator(base=5))
 
 
@@ -71,6 +65,6 @@ fig.text(
     fontsize=9,
 )
 
-plt.tight_layout()
+plt.tight_layout(rect=[0, 0.025, 1, 1])
 plt.savefig(OUT_PATH, dpi=300, facecolor=fig.get_facecolor())
 plt.show()
