af61d172a989c731c1d1411cf273d6171b78cf56
greggs-patriot <rests_fours_0r@icloud.com>
2026-02-21 23:53:02 +0000

    slight fix

--- lords_by_party/6/plot.py
+++ lords_by_party/6/plot.py
@@ -13,17 +13,10 @@ df = pd.read_csv(IN_PATH)
 
 
 df['date'] = pd.to_datetime(df['date'])
-#df = df[(df['date'] >= '2000-01-01') & (df['date'] <= '2025-12-31')]
 
-
-fig, ax, ax_right = ppar.default_settings()
+fig, ax, ax_right = ppar.pre_settings()
 
 ax.plot(df['date'], df['total'], label='Total', color='#c00000', linewidth=1.5)
-ax_right.set_ylim(ax.get_ylim())
-
-
-ax.set_title('House of Lords Membership numbers', x=0.03, loc='left',fontsize=24)
-
 
 ticks = pd.date_range(
     start=df['date'].min(),
@@ -31,22 +24,10 @@ ticks = pd.date_range(
     freq='5YS'   # 5-year start frequency
 )
 
+
 ax.set_xticks(ticks)
 ax.set_xticklabels([d.year for d in ticks])
 
+ppar.post_settings(fig,ax,ax_right)
 
-ax.tick_params(axis='x', labelsize=24)
-
-
-fig.text(
-    0.5, 0.01,
-    "Source: UK Parliament – Members' Names Data Platform (Lords by membership type); "
-    "Elections & Polling Team, Trafalgar Analytics",
-    ha='center',
-    va='bottom',
-    fontsize=9,
-)
-
-plt.tight_layout(rect=[0, 0.025, 1, 1])
-plt.show()
 plt.savefig(OUT_PATH, dpi=300, facecolor=fig.get_facecolor())
--- lords_by_party/6/plot_params.py
+++ lords_by_party/6/plot_params.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
 
 
-def default_settings():
+def pre_settings():
     fig, ax = plt.subplots(figsize=(10, 6))
     bg_colour = '#fff9f5'
+
+    ax.set_title('House of Lords Membership numbers', x=0.03, loc='left',fontsize=24)
     
     fig.patch.set_facecolor(bg_colour)
     ax.set_facecolor(bg_colour)
 
     ax.spines['top'].set_visible(False)
     
-    ax.margins(x=0.025)
-
     ax_right = ax.twinx()
-    
-
     ax_right.spines['top'].set_visible(False)
 
 
@@ -24,6 +22,24 @@ def default_settings():
     
     ax.tick_params(axis='y', labelsize=20)
     ax_right.tick_params(axis='y', labelsize=20)
-    ax.tick_params(axis='x', labelsize=20)
+
+    ax.tick_params(axis='x', labelsize=24)
 
     return fig,ax,ax_right
+
+def post_settings(fig : plt.Figure,ax : plt.Axes, ax_right : plt.Axes):
+    
+
+    ax_right.set_ylim(ax.get_ylim())
+    ax.margins(x=0.025)
+    
+    fig.text(
+        0.5, 0.01,
+        "Source: UK Parliament – Members' Names Data Platform (Lords by membership type); "
+        "Elections & Polling Team, Trafalgar Analytics",
+        ha='center',
+        va='bottom',
+        fontsize=9,
+    )
+
+    plt.tight_layout(rect=[0, 0.025, 1, 1])
