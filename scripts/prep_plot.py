07fc51026fdb3f7769ee5b611dd75854109ecf97
greggs-patriot <rests_fours_0r@icloud.com>
2026-02-21 19:02:35 +0000

    add more pre plots

--- scripts/prep_plot.py
+++ scripts/prep_plot.py
@@ -2,13 +2,39 @@ import os
 import pandas as pd
 
 IN_PATH = os.path.join('interim','lords_count.csv')
-OUT_PATH = os.path.join('interim_2','membership_2000_2025.csv')
+OUT_PATH = os.path.join('interim_2','{}.csv')
 
-df = pd.read_csv(IN_PATH)
+def filter(df,**kw):
+    name,start,end,columns = kw['name'],kw['start'],kw['end'],kw['columns']
+    df = df[(df['date'] >= start) & (df['date'] <= end)]
+    df = df[['date'] + columns]
+
+    df.to_csv(OUT_PATH.format(name),index=False)
 
 
-df['date'] = pd.to_datetime(df['date'])
-df = df[(df['date'] >= '2000-01-01') & (df['date'] <= '2025-12-31')]
-df = df[['date','total']]
 
-df.to_csv(OUT_PATH,index=False)
+plots = [
+        {   'name' : 'membership_2000_2025',
+            'start' : '2000-01-01',
+            'end' : '2025-12-31',
+            'columns' : ['total']
+        },
+        {   'name' : 'membership_1999_2025',
+            'start' : '1999-01-01',
+            'end' : '2025-12-31',
+            'columns' : ['total']
+        },
+        {   'name' : 'membership_1960_2025',
+            'start' : '1960-01-01',
+            'end' : '2025-12-31',
+            'columns' : ['total_male','total_female']
+        },
+]
+
+
+main_df = pd.read_csv(IN_PATH)
+
+#df['date'] = pd.to_datetime(df['date'])
+
+for plot in plots:
+    filter(main_df,**plot)
